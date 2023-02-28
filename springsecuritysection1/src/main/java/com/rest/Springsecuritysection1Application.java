455c953c32c29b8e3b6bc78c1d97d82ea96d9abd
IppeVenkataswamy <ippe.venkataswamy@gmail.com>
2023-02-28 16:57:59 +0530

    Create a simple Spring security project & secure Rest service

---
+++ springsecuritysection1/src/main/java/com/rest/Springsecuritysection1Application.java
@@ -0,0 +1,13 @@
+package com.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springsecuritysection1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springsecuritysection1Application.class, args);
+	}
+
+}
