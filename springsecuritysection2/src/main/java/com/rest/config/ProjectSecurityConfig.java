b63e449b5b203db6fa898986edbf140f61654de9
IppeVenkataswamy <ippe.venkataswamy@gmail.com>
2023-03-01 17:08:54 +0530

    Custom Security Configurations, PermitAll, DenyAll Configurations

---
+++ springsecuritysection2/src/main/java/com/rest/config/ProjectSecurityConfig.java
@@ -0,0 +1,50 @@
+package com.rest.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class ProjectSecurityConfig {
+
+	@Bean
+	SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+		
+        /**
+         *  Below is the custom security configurations
+         */
+		 http.authorizeHttpRequests()
+         .requestMatchers("/myAccount","/myBalance","/myLoans","/myCards").authenticated()
+         .requestMatchers("/notices","/contact").permitAll()
+         .and().formLogin()
+         .and().httpBasic();
+		 return http.build();
+		 
+		
+		/**
+         *   authenticate all the requests
+         */
+		/*http.authorizeHttpRequests().anyRequest().authenticated()
+        .and().formLogin()
+        .and().httpBasic();
+		return http.build();*/
+		
+        /**
+         *  Configuration to permit all the requests
+         */
+        /*http.authorizeHttpRequests().anyRequest().permitAll()
+                .and().formLogin()
+                .and().httpBasic();
+        return http.build();*/
+		
+
+        /**
+         *  Configuration to deny all the requests
+         */
+        /*http.authorizeHttpRequests().anyRequest().denyAll()
+                .and().formLogin()
+                .and().httpBasic();
+        return http.build();*/
+	}
+}
