1b1744c94896882a0ac564c906c7c34ae4c54977
IppeVenkataswamy <ippe.venkataswamy@gmail.com>
2023-03-09 17:22:15 +0530

    Password management using PasswordEncoders

---
+++ springsecuritysection4/src/main/java/com/rest/config/ProjectSecurityConfig.java
@@ -0,0 +1,99 @@
+package com.rest.config;
+import org.springframework.security.core.userdetails.User;
+
+import javax.sql.DataSource;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class ProjectSecurityConfig {
+
+	@Bean
+	SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+		
+		http.csrf().disable()
+        /**
+         *  Below is the custom security configurations
+         */
+		 .authorizeHttpRequests()
+         .requestMatchers("/myAccount","/myBalance","/myLoans","/myCards").authenticated()
+         .requestMatchers("/notices","/contact","/register").permitAll()
+         .and().formLogin()
+         .and().httpBasic();
+		 return http.build();
+		 
+		
+		/**
+         *   authenticate all the requests
+         */
+		/*http.authorizeHttpRequests().anyRequest().authenticated()
+        .and().formLogin()
+        .and().httpBasic();
+		return http.build();*/
+		
+        /**
+         *  Configuration to permit all the requests
+         */
+        /*http.authorizeHttpRequests().anyRequest().permitAll()
+                .and().formLogin()
+                .and().httpBasic();
+        return http.build();*/
+		
+
+        /**
+         *  Configuration to deny all the requests
+         */
+        /*http.authorizeHttpRequests().anyRequest().denyAll()
+                .and().formLogin()
+                .and().httpBasic();
+        return http.build();*/
+	}
+	
+    /*@Bean
+    public InMemoryUserDetailsManager userDetailsService() {
+        *//*Approach 1 where we use withDefaultPasswordEncoder() method
+		while creating the user details*/
+        /*UserDetails admin = User.withDefaultPasswordEncoder()
+                .username("admin")
+                .password("12345")
+                .authorities("admin")
+                .build();
+        UserDetails user = User.withDefaultPasswordEncoder()
+                .username("user")
+                .password("12345")
+                .authorities("read")
+                .build();
+        return new InMemoryUserDetailsManager(admin, user);*/
+         /*Approach 2 where we use NoOpPasswordEncoder Bean
+		while creating the user details*/
+		/*UserDetails admin = User.withUsername("admin")
+                .password("12345")
+                .authorities("admin")
+                .build();
+        UserDetails user = User.withUsername("user")
+                .password("12345")
+                .authorities("read")
+                .build();
+        return new InMemoryUserDetailsManager(admin, user);
+    }*/
+	
+	/*@Bean
+    public UserDetailsService userDetailsService(DataSource dataSource) {
+        return new JdbcUserDetailsManager(dataSource);
+    }*/
+    
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
